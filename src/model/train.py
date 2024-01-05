ca353031c565d2394228c07bac81124cc675ec26
Dmytro H <odeskdima@gmail.com>
2024-01-05 15:53:01 +0200

    fixing styles

--- src/model/train.py
+++ src/model/train.py
@@ -33,10 +33,12 @@ def get_csvs_df(path):
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
+
 def split_data(df):
-    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI',
-               'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin',
+               'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
     return train_test_split(X, y, test_size=0.30, random_state=0)
+
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
@@ -58,6 +60,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
